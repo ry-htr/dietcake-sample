50da54b7684d110813ca44f22e5ab3556363f176
neet <lagoon1981@hotmail.com>
2012-09-22 19:59:11 +0900

    コメントを書き込めるようにした

--- app/models/thread.php
+++ app/models/thread.php
@@ -1,17 +1,27 @@
 <?php
 class Thread extends AppModel
 {
+    /**
+     * get all thread
+     * @return <Thread> array
+     */
     public static function getAll()
     {
         $threads = array();
         $db = DB::conn();
         $rows = $db->rows('SELECT * FROM thread');
+
         foreach ($rows as $row) {
             $threads[] = new Thread($row);
         }
         return $threads;
     }
 
+    /**
+     * get thread that find by id
+     * @param string id
+     * @return Thread
+     */
     public static function get($id)
     {
         $db = DB::conn();
@@ -19,6 +29,10 @@ class Thread extends AppModel
         return new self($row);
     }
 
+    /**
+     * get all comment that find by thread_id
+     * @return <Comment> array
+     */
     public function getComments()
     {
         $comments = array();
@@ -32,4 +46,18 @@ class Thread extends AppModel
         }
         return $comments;
     }
+
+    /**
+     * write comment
+     * @param Comment
+     */
+    public function write(Comment $comment)
+    {
+        $db = DB::conn();
+        $db->query(
+          'INSERT INTO comment SET thread_id = ?, username = ?, body = ?, created = NOW()',
+            array($this->id, $comment->username, $comment->body)
+        );
+    }
+
 }
